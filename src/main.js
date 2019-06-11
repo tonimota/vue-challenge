be3b8067a7a81b7b75cc7041b3b6965e4314fc09
Tonielton Mota <toni.elton1993@gmail.com>
2019-06-10 23:14:05 -0300

    [FEAT] Add buefy style inside instance vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(buefy)
 
 Vue.config.productionTip = false
 
