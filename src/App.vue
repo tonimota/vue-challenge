4804071b559bb4d9752e63f63dcc012bc14e2090
Tonielton Mota <toni.elton1993@gmail.com>
2019-06-10 23:13:37 -0300

    [STYLE] Reset margin and padding from body

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@
 </template>
 
 <style lang="scss">
+html, body {
+  padding: 0px;
+  margin: 0px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
