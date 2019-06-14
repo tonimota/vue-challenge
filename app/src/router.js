8c3cdc14a684d7cd96880b9761056acce36fcb21
Tonielton Mota <toni.elton1993@gmail.com>
2019-06-14 15:02:39 -0300

    [FEAT] Create route for cart

--- app/src/router.js
+++ app/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Cart from './views/Cart.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: Cart
     }
   ]
 })
