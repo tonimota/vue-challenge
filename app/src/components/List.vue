cb4fba6624d5ad467b42df90ca154de098074056
Tonielton Mota <toni.elton1993@gmail.com>
2019-06-14 15:07:07 -0300

    [FEAT] Create list itens for cart

--- app/src/components/List.vue
+++ app/src/components/List.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="list">
-    <Card v-for="(item, index) in data" :key="index" />
+    <div class="list-icon">
+      <img src="@/assets/img/bag.png" alt="Carrinho de Produtos" title="Carrinho de Produtos">
+      <span class="list-icon-title">Carrinho</span>
+    </div>
+    <ListItem v-for="(item, index) in data" :key="index" :item="item" @update-cart="updateCart"/>
   </div>
 </template>
 
 <script>
-import Card from './Card'
+import ListItem from '@/components/ListItem'
 
 export default {
   props: {
@@ -15,15 +19,33 @@ export default {
     }
   },
   components: {
-    Card
+    ListItem
+  },
+  data () {
+    return {
+      itens: []
+    }
+  },
+  methods: {
+    updateCart (e) {
+      this.$emit('delete-item', e)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
   .list {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
+    max-width: 400px;
+    padding: 20px;
+    min-height: 300px;
+    margin: 0 auto;
+    background-color: #202025;
+    &-icon {
+      &-title {
+        display: block;
+        color: #fff;
+      }
+    }
   }
 </style>
