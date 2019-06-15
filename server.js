edc8c8639337740078eba645e641ccd7695707f3
Tonielton Mota <toni.elton1993@gmail.com>
2019-06-14 23:18:58 -0300

    [CHORE] add express and update server

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-const express = require('express')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const app = express()
-const port = process.env.PORT || 3000
-const router = express.Router()
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-app.use('/static', express.static(__dirname + '/dist/static'))
-
-app.engine('.html', require('ejs').renderFile)
-
-app.set('views', `${__dirname}/dist`)
-
-router.get('*', (req, res, next) => {
-  res.sendFile(`${__dirname}/dist/index.html`)
-})
-
-app.use('/', router)
-
+var port = process.env.PORT || 5000
 app.listen(port)
-console.log('app running on port', port)
+
+console.log('server started ' + port)
