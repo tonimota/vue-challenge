cebfafbd50b3273384d32e965349ea81bb74b1d0
Tonielton Mota <toni.elton1993@gmail.com>
2019-06-14 21:20:06 -0300

    [FEAT] Created bottom card detail

---
+++ app/src/components/ListTotal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="total-cart">
+    <p class="total-cart-description">
+      <span class="total-cart-description-title">Subtotal</span>
+      <span class="total-cart-description-total">R$ <strong>{{cart.totalOrder}}</strong><em>ou em até 10x de R$ {{cart.totalInstallments}}</em></span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    cart: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .total-cart {
+    height: 60px;
+    &-description {
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      align-items: center;
+      color: #fff;
+      font-size: 12px;
+      &-total {
+        strong {
+          font-size: 18px;
+          color: #dfbd00;
+        }
+        em {
+          display: block;
+        }
+      }
+    }
+  }
+</style>
