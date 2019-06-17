fb71574be6c077ff568a8030a7da8390ef199148
Tonielton Mota <toni.elton1993@gmail.com>
2019-06-17 09:58:44 -0300

    [CHORE] Remove timeout

--- src/service/http.js
+++ src/service/http.js
@@ -3,8 +3,7 @@ import axios from 'axios'
 const url = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://api-list-simple.herokuapp.com'
 
 const products = axios.create({
-  baseURL: url,
-  timeout: 3000
+  baseURL: url
 })
 
 export default products
